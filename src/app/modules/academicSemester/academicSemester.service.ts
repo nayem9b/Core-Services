20651898b6950f181a8807ea62819cea0afdc1bd
nayem9b <razwanniam1@gmail.com>
2023-08-21 00:53:29 +0600

    searchTerm and Condition inplemented

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,7 +1,8 @@
-import { AcademicSemester, PrismaClient } from '@prisma/client';
+import { AcademicSemester, Prisma, PrismaClient } from '@prisma/client';
 import { paginationHelpers } from '../../../helpers/paginationHelper';
 import { IGenericResponse } from '../../../interfaces/common';
 import { IPaginationOptions } from '../../../interfaces/pagination';
+import { IAcademicSemesterFilterRequest } from './academicSemester.interface';
 
 const prisma = new PrismaClient();
 
@@ -16,11 +17,31 @@ const insertIntoDb = async (
 };
 
 const getAllFromDB = async (
-  filters,
+  filters: IAcademicSemesterFilterRequest,
   options: IPaginationOptions
 ): Promise<IGenericResponse<AcademicSemester[]>> => {
   const { page, limit, skip } = paginationHelpers.calculatePagination(options);
+
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: ['title', 'code', 'startMonth', 'endMonth'].map(field => ({
+        [field]: {
+          contains: searchTerm,
+          mode: 'insensitive',
+        },
+      })),
+    });
+  }
+
+  const whereConditions: Prisma.AcademicSemesterWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
   const result = await prisma.academicSemester.findMany({
+    where: whereConditions,
     skip,
     take: limit,
   });
